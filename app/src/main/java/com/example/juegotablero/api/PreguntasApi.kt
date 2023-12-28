97a06bedffee5c25299e2d043fb49b2d0fdf17a4
Moisés M <moiseselquimico@gmail.com>
2023-12-28 16:27:31 +0000

    conexion con api

---
+++ app/src/main/java/com/example/juegotablero/api/PreguntasApi.kt
@@ -0,0 +1,98 @@
+import android.util.Log
+import com.example.juegotablero.model.Pregunta
+import com.google.firebase.database.*
+
+class PreguntasApi {
+    private val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().reference
+    private var preguntasRepaso: List<Pregunta.Repaso> = emptyList()
+    private var preguntasAdivinaPalabra: List<Pregunta.AdivinaPalabra> = emptyList()
+    private var preguntasTest: List<Pregunta.Test> = emptyList()
+    private var preguntasJuegoParejas: List<Pregunta.JuegoParejas> = emptyList()
+    private var preguntasPruebaFinal: List<Pregunta.PruebaFinal> = emptyList()
+
+    init {
+        obtenerPreguntasRepaso()
+        obtenerPreguntasAdivinaPalabra()
+
+    }
+
+    private fun obtenerPreguntasRepaso() {
+        val repasoReference = databaseReference.child("repaso")
+        repasoReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                preguntasRepaso = dataSnapshot.children.mapNotNull { it.getValue(Pregunta.Repaso::class.java) }
+                mostrarPreguntasRepaso()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Maneja los errores de lectura de la base de datos
+
+            }
+        })
+    }
+    private fun obtenerPreguntasAdivinaPalabra() {
+        val adivinaPalabraReference = databaseReference.child("adivina_palabra")
+        adivinaPalabraReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                preguntasAdivinaPalabra = dataSnapshot.children.mapNotNull { it.getValue(Pregunta.AdivinaPalabra::class.java) }
+                mostrarPreguntasAdivinaPalabra()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Maneja los errores de lectura de la base de datos
+            }
+        })
+    }
+    private fun obtenerPreguntasTest() {
+        val testReference = databaseReference.child("test")
+        testReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                preguntasTest = dataSnapshot.children.mapNotNull { it.getValue(Pregunta.Test::class.java) }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Maneja los errores de lectura de la base de datos
+            }
+        })
+    }
+
+    private fun obtenerPreguntasJuegoParejas() {
+        val juegoParejasReference = databaseReference.child("juego_parejas")
+        juegoParejasReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                preguntasJuegoParejas = dataSnapshot.children.mapNotNull { it.getValue(Pregunta.JuegoParejas::class.java) }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Maneja los errores de lectura de la base de datos
+            }
+        })
+    }
+
+    private fun obtenerPreguntasPruebaFinal() {
+        val pruebaFinalReference = databaseReference.child("prueba_final")
+        pruebaFinalReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                preguntasPruebaFinal = dataSnapshot.children.mapNotNull { it.getValue(Pregunta.PruebaFinal::class.java) }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Maneja los errores de lectura de la base de datos
+            }
+        })
+    }
+
+    private fun mostrarPreguntasRepaso() {
+        for (pregunta in preguntasRepaso) {
+            Log.d("Pregunta", "Repaso: ${pregunta.enunciado}")
+        }
+    }
+
+    private fun mostrarPreguntasAdivinaPalabra() {
+        for (pregunta in preguntasAdivinaPalabra) {
+            Log.d("Pregunta", "Adivina palabra: ${pregunta.definicion}")
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/juegotablero/model/Preguntas.kt
+++ app/src/main/java/com/example/juegotablero/model/Preguntas.kt
@@ -3,59 +3,42 @@ package com.example.juegotablero.model
 data class PreguntasContainer(
     val preguntas: List<Pregunta>
 )
-//sea class sirve para definir una clase que puede tener varios tipos de datos dentro de ella
-//y podremosevaluar con un when que tipo de dato es y hacer algo con el.
-sealed class Pregunta {
 
+sealed class Pregunta {
     data class Repaso(
-        val enunciado: String,
-        val opciones: List<String>,
-        val respuestaCorrecta: String
+        val enunciado: String = "",
+        val opciones: List<String> = emptyList(),
+        val respuesta_correcta: String = ""
     ) : Pregunta()
 
     data class AdivinaPalabra(
-        val definicion: String,
-        val palabra: String
+        val tipo: String = "",
+        val definicion: String = "",
+        val palabra: String = ""
     ) : Pregunta()
 
     data class Test(
-        val enunciado: String,
-        val opciones: List<String>,
-        val respuestaCorrecta: String
+        val tipo: String = "",
+        val enunciado: String = "",
+        val opciones: List<String> = emptyList(),
+        val respuestaCorrecta: String = ""
     ) : Pregunta()
 
     data class JuegoParejas(
-        val parejas: List<Pareja>
+        val tipo: String = "",
+        val parejas: List<Pareja> = emptyList()
     ) : Pregunta()
 
     data class PruebaFinal(
-        val enunciado: String,
-        val opciones: List<String>,
-        val respuestaCorrecta: String
+        val tipo: String = "",
+        val enunciado: String = "",
+        val opciones: List<String>? = emptyList(),
+        val respuestaCorrecta: String? = "",
+        val respuestaCorrectaPalabrasClave: String? = ""
     ) : Pregunta()
 
-    fun evaluarPregunta(pregunta: Pregunta) {
-        when (pregunta) {
-            is Pregunta.Repaso -> {
-
-            }
-            is Pregunta.AdivinaPalabra -> {
-
-            }
-            is Pregunta.Test -> {
-
-            }
-            is Pregunta.JuegoParejas -> {
-
-            }
-            is Pregunta.PruebaFinal -> {
-
-            }
-        }
-    }
+    data class Pareja(
+        val opcion1: String = "",
+        val opcion2: String = ""
+    )
 }
-
-data class Pareja(
-    val opcion1: String,
-    val opcion2: String
-)
